036432e0aadf5ae5097434dd68298981449cd671
Bernard Polidario <bernard@weeklyhow.com>
2025-02-25 16:01:17 +0100

    ✨ Update About and Work pages to indicate they are under construction and improve styling

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,15 +1,24 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>This page is still under construction</h1>
   </div>
 </template>
 
 <style>
 @media (min-width: 1024px) {
   .about {
+    width: 100%;
     min-height: 100vh;
     display: flex;
     align-items: center;
+  }
+
+  .about h1 {
+    font-size: 2rem;
+    font-weight: bold;
+    text-align: center;
+    width: 100%;
+    color: var(--color-primary);
   }
 }
 </style>
--- src/views/Work.vue
+++ src/views/Work.vue
@@ -1,16 +1,22 @@
 <template>
-    <div class="work">
-      <h1>This is the work page</h1>
-    </div>
-  </template>
+  <div class="work">
+    <h1>This page is still under construction</h1>
+  </div>
+</template>
   
-  <style>
-  @media (min-width: 1024px) {
-    .work {
-      min-height: 100vh;
-      display: flex;
-      align-items: center;
-    }
-  }
-  </style>
+<style>
+.work {
+  min-height: 100vh;
+  display: flex;
+  align-items: center;
+}
+
+.work h1 {
+  font-size: 2rem;
+  font-weight: bold;
+  text-align: center;
+  width: 100%;
+  color: var(--color-primary);
+}
+</style>
   
