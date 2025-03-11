1bdf2155bdcfe496326eded4e3c9363380b8435b
Bernard Polidario <bernard@weeklyhow.com>
2025-03-11 19:06:14 +0100

    ✨Fix import path casing for Homepage component in router configuration

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Homepage from '../views/HomePage.vue'
+import Homepage from '../views/Homepage.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
