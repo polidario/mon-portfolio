4352dabd4b847254be79398c47e53bf8dfcc5f12
Bernard Polidario <bernard@weeklyhow.com>
2025-04-04 13:28:58 +0200

    🔄 Refactor `404Page` component to improve template structure and update styling to SCSS

--- src/views/404Page.vue
+++ src/views/404Page.vue
@@ -19,24 +19,24 @@ onMounted(() => {
 </script>
 
 <template>
-    <div class="page-404">
-      <h1>Page not found</h1>
-    </div>
-  </template>
+  <div class="page-404">
+    <h1>Page not found</h1>
+  </div>
+</template>
     
-  <style>
-  .page-404 {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-  
-  .page-404 h1 {
+<style lang="scss">
+.page-404 {
+  min-height: 100vh;
+  display: flex;
+  align-items: center;
+
+  h1 {
     font-size: 2rem;
     font-weight: bold;
     text-align: center;
     width: 100%;
     color: var(--color-primary);
   }
-  </style>
+}
+</style>
     
