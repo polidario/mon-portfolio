78d7ac8652198d8d52a1eb8f7111651712bb6f24
Bernard Polidario <bernard@weeklyhow.com>
2025-04-04 13:36:17 +0200

    🔄 Update `404Page` component to include a descriptive message and enhance styling for better user experience

--- src/views/404Page.vue
+++ src/views/404Page.vue
@@ -21,6 +21,7 @@ onMounted(() => {
 <template>
   <div class="page-404">
     <h1>Page not found</h1>
+    <p>The page you are looking for does not exist.</p>
   </div>
 </template>
     
@@ -28,14 +29,24 @@ onMounted(() => {
 .page-404 {
   min-height: 100vh;
   display: flex;
+  flex-direction: column;
+  justify-content: center;
   align-items: center;
+
+  gap: var(--space-5);
 
   h1 {
     font-size: 2rem;
     font-weight: bold;
     text-align: center;
     width: 100%;
-    color: var(--color-primary);
+    color: var(--color-white);
+  }
+
+  p {
+    font-size: 1.25rem;
+    text-align: center;
+    color: var(--color-gray-500);
   }
 }
 </style>
