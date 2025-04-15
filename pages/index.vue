dcabf59ebcdb94fa77976545f07475dfd20e1645
Bernard Polidario <bernard@weeklyhow.com>
2025-04-15 22:43:43 +0200

    ✨ Update homepage layout by integrating image comparison, parallax images, and hero section components

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,12 @@
 <template>
-    <div>
-        <h1>Welcome to the Home Page</h1>
-        <p>This is the main content of the home page.</p>
+    <div class="homepage d-flex flex-column align-center justify-center ga-16">
+        <div :style="{ maxWidth: '1080px', zIndex: 1 }" id="imageComparison">
+            <HomepageImageComparison />
+        </div>
+
+        <HomepageParallaxImages />
+
+        <HomepageHeroSection />
     </div>
+    
 </template>
