f19c841b9250d19854c5f1ca503b3e6e6b4649ef
Bernard Polidario <bernard@weeklyhow.com>
2025-04-15 17:48:53 +0200

    ✨ Update nuxt.config.ts to include custom element support for 'motion'; add css-element-queries dependency in package.json

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,11 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   ssr: true,
+  vue: {
+    compilerOptions: {
+      isCustomElement: (tag) => ['motion'].includes(tag),
+    },
+  },
 
   css: [
     '@/assets/css/main.scss',
@@ -11,12 +16,12 @@ export default defineNuxtConfig({
     redirect: false,
   },
   modules: [
+    'motion-v/nuxt',
     '@nuxt/image',
     '@nuxt/icon',
     '@nuxt/fonts',
     '@nuxt/eslint',
     '@nuxt/test-utils',
     '@nuxtjs/supabase',
-    'motion-v/nuxt'
   ]
 })
