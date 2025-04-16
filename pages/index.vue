74d8a1207235f29daaa67ce63e2e8e05b5be669f
Bernard Polidario <bernard@weeklyhow.com>
2025-04-16 15:16:24 +0200

    🚀 Add `TimelineVertical` component and integrate it into the homepage layout

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,9 @@
+<script setup lang="ts">
+import type { TimelineItem } from '~/types/components';
+
+const { data, error } = await useFetch<TimelineItem[]>('/api/experiences');
+if (error.value) console.error('Error fetching experiences:', error.value);
+</script>
 <template>
     <div class="homepage d-flex flex-column align-center justify-center ga-16">
         <div :style="{ maxWidth: '1080px', zIndex: 1 }" id="imageComparison">
@@ -7,6 +13,6 @@
         <HomepageParallaxImages />
 
         <HomepageHeroSection />
+        <HomepageTimelineVertical :items="data || []" />
     </div>
-    
 </template>
