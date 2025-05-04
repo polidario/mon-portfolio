15d6c4a267a7481d000d4b916255ae0b433e8df1
Bernard Polidario <bernard@weeklyhow.com>
2025-05-04 16:12:12 +0200

    ✨ Update nuxt.config.ts: remove @nuxt/content module and format code for improved readability

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,24 +1,29 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   ssr: true,
+
   vue: {
     compilerOptions: {
       isCustomElement: (tag) => ['motion'].includes(tag),
     },
   },
+
   css: ['@/assets/css/main.scss'],
+
   supabase: {
     redirect: false,
   },
+
   modules: [
     'motion-v/nuxt',
     '@nuxt/image',
     '@nuxt/icon',
-    '@nuxt/content',
     '@nuxt/fonts',
     '@nuxt/eslint',
     '@nuxt/test-utils',
     '@nuxtjs/supabase',
     '@nuxtjs/device'
-  ]
+  ],
+
+  compatibilityDate: '2025-05-04'
 })
