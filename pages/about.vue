f83576c92c4251d5bcf33cb431db5c0f91ae25e3
Bernard Polidario <bernard@weeklyhow.com>
2025-05-14 02:34:36 +0200

    🗑️ Remove About and Work pages

--- pages/about.vue
+++
@@ -1,7 +0,0 @@
-<template>
-    <div>
-        <h1>Welcome to the About Page</h1>
-        <p>This is the main content of the about page.</p>
-        <p>Here you can find information about our company, mission, and values.</p>
-    </div>
-</template>
--- pages/work.vue
+++
@@ -1,6 +0,0 @@
-<template>
-    <div>
-        <h1>Welcome to the Work Page</h1>
-        <p>This is the main content of the work page.</p>
-    </div>
-</template>
