570651971c820643654f8fa2bc2571e61590710c
Bernard Polidario <bernard@weeklyhow.com>
2025-07-14 15:40:49 +0200

    ✨ Add dynamic page rendering with GraphQL data fetching

---
+++ pages/[slug].vue
@@ -0,0 +1,20 @@
+<script lang="ts" setup>
+import { contentQuery } from "~/queries/content";
+import type { GraphQLResponse } from "~/types/page"
+
+const route = useRoute()
+const slug = route.params.slug
+
+const graphql = useStrapiGraphQL()
+
+const result = await graphql<GraphQLResponse>(contentQuery, { "Slug": slug })
+const data = result.data;
+
+console.log(data.pages)
+</script>
+
+<template>
+    <div class="layout">
+        <ContentGrid :content="data.pages[0].content"/>
+    </div>
+</template>
