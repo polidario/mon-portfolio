1432674395b8cadbbf78304901af1db549002dc8
Bernard Polidario <bernard@weeklyhow.com>
2025-07-14 15:40:58 +0200

    ✨ Enhance GraphQL query to include additional component sections and fields for improved content structure

--- queries/content.ts
+++ queries/content.ts
@@ -12,13 +12,157 @@ query Pages($Slug: String!){
       ... on ComponentSectionsHeadline {
         __typename
         id
-        title
+        headlineTitle: title
         subtitle
       }
 
       ... on ComponentSectionsImage {
         __typename
         id
+        image {
+          url
+          alternativeText
+          width
+          height
+          name
+        }
+      }
+
+      ... on ComponentSectionsTextBlock {
+        __typename
+        id
+        content
+        textAlign
+        textSize
+      }
+
+      ... on ComponentSectionsButton {
+        __typename
+        id
+        text
+        url
+        isExternal
+        variant
+        size
+        fullWidth
+      }
+
+      ... on ComponentSectionsCard {
+        __typename
+        id
+        cardTitle: title
+        description
+        linkText
+        linkUrl
+        linkIsExternal
+        imagePosition
+        image {
+          url
+          alternativeText
+          width
+          height
+          name
+        }
+      }
+
+      ... on ComponentSectionsTestimonial {
+        __typename
+        id
+        quote
+        authorName
+        authorTitle
+        authorCompany
+        rating
+        authorImage {
+          url
+          alternativeText
+          width
+          height
+          name
+        }
+      }
+
+      ... on ComponentSectionsGrid {
+        __typename
+        id
+        columns
+        gap
+        alignment
+        containerClass
+        blocks {
+          ... on ComponentSectionsHeadline {
+            __typename
+            id
+            headlineTitle: title
+            subtitle
+          }
+
+          ... on ComponentSectionsImage {
+            __typename
+            id
+            image {
+              url
+              alternativeText
+              width
+              height
+              name
+            }
+          }
+
+          ... on ComponentSectionsTextBlock {
+            __typename
+            id
+            content
+            textAlign
+            textSize
+          }
+
+          ... on ComponentSectionsButton {
+            __typename
+            id
+            text
+            url
+            isExternal
+            variant
+            size
+            fullWidth
+          }
+
+          ... on ComponentSectionsCard {
+            __typename
+            id
+            cardTitle: title
+            description
+            linkText
+            linkUrl
+            linkIsExternal
+            imagePosition
+            image {
+              url
+              alternativeText
+              width
+              height
+              name
+            }
+          }
+
+          ... on ComponentSectionsTestimonial {
+            __typename
+            id
+            quote
+            authorName
+            authorTitle
+            authorCompany
+            rating
+            authorImage {
+              url
+              alternativeText
+              width
+              height
+              name
+            }
+          }
+        }
       }
     }
   }
